ea4d307588b3536168c7e99e0a807725aa6d681c
Dário <dario.alef@gmail.com>
2025-05-29 20:33:37 -0400

    CRUD - Dário

--- app/views.py
+++ app/views.py
@@ -1,35 +1,69 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Item
-from .forms import ItemForm
+from .models import Category
+from .forms import CategoryForm
 
-def item_list(request):
-    items = Item.objects.all()
-    return render(request, 'app/item_list.html', {'items': items})
+# def item_list(request):
+#     items = Item.objects.all()
+#     return render(request, 'app/item_list.html', {'items': items})
 
-def item_create(request):
+# def item_create(request):
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('item_list')
+#     else:
+#         form = ItemForm()
+#     return render(request, 'app/item_form.html', {'form': form})
+
+# def item_update(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST, instance=item)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('item_list')
+#     else:
+#         form = ItemForm(instance=item)
+#     return render(request, 'app/item_form.html', {'form': form})
+
+# def item_delete(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     if request.method == 'POST':
+#         item.delete()
+#         return redirect('item_list')
+#     return render(request, 'app/item_confirm_delete.html', {'item': item})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'app/category_list.html', {'categories': categories})
+
+def category_create(request):
     if request.method == 'POST':
-        form = ItemForm(request.POST)
+        form = CategoryForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('item_list')
+            return redirect('category_list')
     else:
-        form = ItemForm()
-    return render(request, 'app/item_form.html', {'form': form})
+        form = CategoryForm()
+    return render(request, 'app/category_form.html', {'form': form})
 
-def item_update(request, pk):
-    item = get_object_or_404(Item, pk=pk)
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item)
+        form = CategoryForm(request.POST, instance=category)
         if form.is_valid():
             form.save()
-            return redirect('item_list')
+            return redirect('category_list')
     else:
-        form = ItemForm(instance=item)
-    return render(request, 'app/item_form.html', {'form': form})
+        form = CategoryForm(instance=category)
+    return render(request, 'app/category_form.html', {'form': form})
 
-def item_delete(request, pk):
-    item = get_object_or_404(Item, pk=pk)
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
-        item.delete()
-        return redirect('item_list')
-    return render(request, 'app/item_confirm_delete.html', {'item': item})
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'app/category_confirm_delete.html', {'category': category})
