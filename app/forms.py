d682dec17a8822aca9e2419de1451ea0aa57a47f
thsilvadev <thsilva.developer@gmail.com>
2025-05-29 21:17:31 -0400

    Aqui, será necessário adicionar à lista 'fields' o campo correspondente (autor, editora) conforme eles forem implementados, para que esses campos apareçam na hora de adicionar ou editar um livro no html

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from .models import Item, Categoria
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'categoria']
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
