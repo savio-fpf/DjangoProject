7472d0345b712b494b160abf89b154494d98ebed
thsilvadev <thsilva.developer@gmail.com>
2025-05-29 21:14:02 -0400

    Esse é o padrão urls.py que funcionou. Atente-se que o item_list é o 'Home' do app, então ele fica no caminho ''. O restante deve seguir o padrão de ##categoria

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     ##item
     path('', views.item_list, name='item_list'),
-    path('/item/new', views.item_create, name='item_create'),
-    path('/item/edit/<int:pk>/', views.item_update, name='item_update'),
-    path('/item/delete/<int:pk>/', views.item_delete, name='item_delete'),
+    path('item/new', views.item_create, name='item_create'),
+    path('item/edit/<int:pk>/', views.item_update, name='item_update'),
+    path('item/delete/<int:pk>/', views.item_delete, name='item_delete'),
     ##categoria
-    path('/categoria', views.categoria_list(), name='categoria_list'),
-    path('/categoria/new/', views.item_create, name='item_create'),
-    path('/categoria/edit/<int:pk>/', views.item_update, name='item_update'),
-    path('/categoria/delete/<int:pk>/', views.item_delete, name='item_delete'),
+    path('categoria/', views.categoria_list, name='categoria_list'),
+    path('categoria/new/', views.categoria_create, name='categoria_create'),
+    path('categoria/edit/<int:pk>/', views.categoria_update, name='categoria_update'),
+    path('categoria/delete/<int:pk>/', views.categoria_delete, name='categoria_delete'),
 ]
