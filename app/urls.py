f96fbe41ebb634de40460bcf3dedd7561bb034bc
dissectedv <joaovictorsampaio393@gmail.com>
2025-05-29 21:29:15 -0400

    feature_all

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
-    path('new/', views.item_create, name='item_create'),
-    path('edit/<int:pk>/', views.item_update, name='item_update'),
-    path('delete/<int:pk>/', views.item_delete, name='item_delete'),
-    path('', views.autor_list, name='autor_list'),
-    path('new/', views.autor_create, name='autor_create'),
-    path('edit/<int:pk>/', views.autor_update, name='autor_update'),
-    path('delete/<int:pk>/', views.autor_delete, name='autor_delete'),
+    # Items
+    path('item/', views.item_list, name='item_list'),
+    path('item/new/', views.item_create, name='item_create'),
+    path('item/edit/<int:pk>/', views.item_update, name='item_update'),
+    path('item/delete/<int:pk>/', views.item_delete, name='item_delete'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('item_list')),  # redireciona "/" para a view 'item_list'
     path('', include('app.urls')),
 ]
