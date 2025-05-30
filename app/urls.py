3dd1680f98173feb33bcc9905325c7907b87c407
thsilvadev <thsilva.developer@gmail.com>
2025-05-29 20:25:18 -0400

    Neste commit estou atualizando o urls.py, editando os caminhos relacionados ao item (colocando eles numa subrota /item, para organizar) e adicionando os caminhos do modelo que estou trabalhando Categoria.

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    ##item
     path('', views.item_list, name='item_list'),
-    path('new/', views.item_create, name='item_create'),
-    path('edit/<int:pk>/', views.item_update, name='item_update'),
-    path('delete/<int:pk>/', views.item_delete, name='item_delete'),
+    path('/item/new', views.item_create, name='item_create'),
+    path('/item/edit/<int:pk>/', views.item_update, name='item_update'),
+    path('/item/delete/<int:pk>/', views.item_delete, name='item_delete'),
+    ##categoria
+    path('/categoria', views.categoria_list(), name='categoria_list'),
+    path('/categoria/new/', views.item_create, name='item_create'),
+    path('/categoria/edit/<int:pk>/', views.item_update, name='item_update'),
+    path('/categoria/delete/<int:pk>/', views.item_delete, name='item_delete'),
 ]
